a67e3030bbd14518fd55262d344bb6cc33060380
tinumu <hirakawa631@i.softbank.jp>
2020-08-06 22:05:59 +0900

    HoT: Initial revision

---
+++ Heads_or_Tails.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cout << "Tossing a coin..." << endl;
+	int heads = 0, tails = 0;
+	
+	srand((unsigned)time(NULL));
+	for (int i = 1; i <= 3; i++) {
+		int r = rand() % 2;
+		cout << "Round " << i << ": ";
+		if (r == 0) {
+			heads++;
+			cout << "Heads" << endl;
+		} else {
+			tails++;
+			cout << "Tails" << endl;
+		}
+	}
+	cout << "Heads: " << heads << ", Tails: " << tails << endl;
+	return (0);
+}
